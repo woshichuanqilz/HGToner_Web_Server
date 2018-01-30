62a24ea9b6d2f2479969beab68a63f5eb4c2d0e7
OctoberNinth <513278236@qq.com>
2018-01-30 23:07:51 +0800

    2018-1-30

--- render_template_main.py
+++ render_template_main.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import random
 import _thread
 import webbrowser
 import ast
@@ -43,24 +44,41 @@ Navbar_title = 'HGToner Sales Dept'
 # navbaritems = ['主页', '信息提交', '信息查询', '表格处理']
 navbaritems = ['Home', 'Info Submit', 'Info Enquiry', 'Table Process']
 
+
+# Set Random Backgroundorder    
+def Set_Random_Background(pick_number):
+    img_list = []
+    path = "./static/images/background/*.*"
+    for fname in glob.glob(path):
+        fname = re.sub(r"\\", "/", fname)
+        if fname:
+            img_list.append(('/'.join(fname.split('/')[2:])))
+
+    bk_list = random.sample(set(img_list), pick_number)
+    return bk_list
+
 def open_browser (url):
     webbrowser.open_new_tab(url)
     # os.system('start ' + url)
 
-# information init
-with open('page_content.txt', encoding='utf-8', errors='ignore') as the_file:
-    page_content_dict = ast.literal_eval(the_file.read())
-
-with open('miscellaneous.txt', encoding='utf-8', errors='ignore') as the_file:
-    miscellaneous = ast.literal_eval(the_file.read())
 
 @app.route('/')
 def index():
     return render_template(html_template_path, page_content = page_content_dict['page_content'], navbaritems = navbaritems, Navbar_title = Navbar_title, navdropdown = miscellaneous['nav_dropdown_items'], nav_dropdown_button_name = miscellaneous['nav_dropdown_button_name'], page_title = page_title)
 
 if __name__ == "__main__":
+# information init
+    with open('page_content.txt', encoding='utf-8', errors='ignore') as the_file:
+        page_content_dict = ast.literal_eval(the_file.read())
+
+    with open('miscellaneous.txt', encoding='utf-8', errors='ignore') as the_file:
+        miscellaneous = ast.literal_eval(the_file.read())
+
+    bk_list = Set_Random_Background(len(page_content_dict['page_content']))
+    for idx, val in enumerate(list(page_content_dict['page_content'].keys())):
+        page_content_dict['page_content'][val]['bk_img_url'] = bk_list[idx]
     # app.run(host = '192.168.0.72', debug = True)
+    # _thread.start_new_thread( open_browser, ('http://' + host + ':' + str(port), ) )
     host = '127.0.0.1'
     port = 5000
-    _thread.start_new_thread( open_browser, ('http://' + host + ':' + str(port), ) )
     app.run(debug = True, host = host, port = port)
